04366bcfb0342496b4e0e3cdcef8582ecbea2650
Combii <davidstovle@gmail.com>
2018-10-10 13:49:40 +0200

    Reformat code

--- Environment.java
+++ Environment.java
@@ -2,24 +2,30 @@ import java.util.HashMap;
 import java.util.Map.Entry;
 
 class Environment {
-    private HashMap<String,Double> variableValues = new HashMap<String,Double>();
-    public Environment() { }	
+    private HashMap<String, Double> variableValues = new HashMap<String, Double>();
+
+    public Environment() {
+    }
+
     public void setVariable(String name, Double value) {
-	variableValues.put(name, value);
+        variableValues.put(name, value);
     }
-    
-    public Double getVariable(String name){
-	Double value = variableValues.get(name); 
-	if (value == null) { System.err.println("Variable not defined: "+name); System.exit(-1); }
-	return value;
+
+    public Double getVariable(String name) {
+        Double value = variableValues.get(name);
+        if (value == null) {
+            System.err.println("Variable not defined: " + name);
+            System.exit(-1);
+        }
+        return value;
     }
-    
+
     public String toString() {
-	String table = "";
-	for (Entry<String,Double> entry : variableValues.entrySet()) {
-	    table += entry.getKey() + "\t-> " + entry.getValue() + "\n";
-	}
-	return table;
-    }   
+        String table = "";
+        for (Entry<String, Double> entry : variableValues.entrySet()) {
+            table += entry.getKey() + "\t-> " + entry.getValue() + "\n";
+        }
+        return table;
+    }
 }
 
